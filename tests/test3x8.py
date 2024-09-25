fb3e3f14584c007de63279428bbfdb831104148b
kamevor7 <kjamevor@unomaha.edu>
2024-09-25 17:22:58 -0500

    update to Lesson

--- tests/test3x8.py
+++ tests/test3x8.py
@@ -14,14 +14,14 @@ for i in code:
             exec("".join(i['source']))
 
 
-class testCases(unittest.TestCase):
+class test3x8(unittest.TestCase):
 
-    def test3x8(self): 
+    def test3x8(self):
       result = '  [ 4 6 1 9 10 12 1 7 ]\n  [ 1 2 3 4 5 6 7 8 ]\n  [ 19 0 3 1 10 15 -1 -1 ]\n\n'
 
       matrixList = [[4,6,1,9,10,12,1,7],
-			  [1,2,3,4,5,6,7,8],
-			  [19,0,3,1,10,15,-1,-1]]
+			        [1,2,3,4,5,6,7,8],
+			        [19,0,3,1,10,15,-1,-1]]
 
       mat2 = Matrix(matrixList)
 
